f88f45d518e97bf45cda1775c4aea8c27fc56edb
coderperl <pelle_gortz@hotmail.com>
2023-01-03 06:58:43 +0100

    full project

---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const base_url = 'https://youtube-v31.p.rapidapi.com'
+const options = {
+    params: {part: 'snippet', videoId: 'M7FIvfx5J10'},
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+  };
+
+  export const fetchFromApi = async (url) => {
+    const {data} = await axios.get(`${base_url}/${url}`, options);
+    return data;
+  }
